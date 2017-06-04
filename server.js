ff226042636a72b11826fd793dfb64578472bbd6
sebtn <sdebedout@gmail.com>
2017-06-04 17:47:25 -0400

    Server to localhost added

---
+++ server.js
@@ -0,0 +1,7 @@
+let express = require('express')
+let app =  = experss()
+
+app.use(express.statis('public'))
+app.listen(3000, serverExpressFn = () => {
+	console.log('Express is now running your //localhost:3000')
+})
