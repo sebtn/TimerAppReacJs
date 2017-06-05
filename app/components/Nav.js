93cd404b888d6e4179794d779ae858d54cc2c699
sebtn <sdebedout@gmail.com>
2017-06-04 23:29:54 -0400

    Nav turned into stateLess component

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -2,30 +2,24 @@ import React, {Component} from 'react'
 import {IndexLink, Link} from 'react-router'
 
 'use strict'
-export default class Nav extends Component {
-	constructor() {
-		super()
-		this.state = {}
-	}
-
-	render() {
-		return (
-			<div className="nav-container">
-				<nav className="nav nav-pills flex-column flex-md-row justify-content-center">
-				<li className="nav-item">
-				  <IndexLink className="flex-sm-fill text-sm-center nav-link title disabled" to="/">Timer App build on ReactJs</IndexLink>
-				</li>
-				  <li className="nav-item">
-				  <IndexLink className="flex-sm-fill text-sm-center nav-link counter" to="/Counter">Counter</IndexLink>
-				  </li>
-				  <li className="nav-item">
-				  <IndexLink className="flex-sm-fill text-sm-center nav-link timer" to="/Timer">Timer</IndexLink>
-				  </li>
-				  <li className="nav-item">
-				  <IndexLink className="flex-sm-fill text-sm-center nav-link author" to="/">Seb made.this</IndexLink>
-				  </li>
-				</nav>
-			</div>
-		)
-	}
+const Nav = () => {
+	return (
+		<div className="nav-container">
+			<nav className="nav nav-pills flex-column flex-md-row justify-content-center">
+			<li className="nav-item">
+			  <IndexLink className="flex-md-fill text-md-center nav-link title disabled" to="/">Timer App build on ReactJs</IndexLink>
+			</li>
+			  <li className="nav-item">
+			  <IndexLink className="flex-md-fill text-md-center nav-link counter" to="/Counter">Counter</IndexLink>
+			  </li>
+			  <li className="nav-item">
+			  <IndexLink className="flex-md-fill text-md-center nav-link timer" to="/Timer">Timer</IndexLink>
+			  </li>
+			  <li className="nav-item">
+			  <IndexLink className="flex-md-fill text-md-center nav-link author" to="/">Author: Seb</IndexLink>
+			  </li>
+			</nav>
+		</div>
+	)
 }
+module.exports = Nav
