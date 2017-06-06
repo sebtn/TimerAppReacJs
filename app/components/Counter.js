756e27dbc45652f6e1974ecff339e777692e60e2
sebtn <sdebedout@gmail.com>
2017-06-06 04:43:13 -0400

    Clock is now counting down

--- app/components/Counter.js
+++ app/components/Counter.js
@@ -8,19 +8,40 @@ export default class Counter extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			count: 0
+			count: 0,
+			countDownStatus: 'stopped'
 		}
 	}
 
 /*-------------------------------------------------------*/
+	startTimer = () => {
+		/*Way to keep re-trigger function after an amount of time*/
+		this.timer = setInterval( () => {
+			let newCount = this.state.count - 1
+			this.setState({ count: newCount >= 0 ? newCount : 0 })
+		}, 1000 ) 
+	}
+
+/*-------------------------------------------------------*/
+	componentDidUpdate = (prevPorps, prevState) => {
+		if(this.state.countDownStatus !== prevState.countDownStatus) {
+			switch(this.state.countDownStatus) {
+				case 'started':
+					this.startTimer()
+					break
+			}
+		}
+	}  
+
+/*-------------------------------------------------------*/
 	handleSetCountDown = (seconds) => {
-		this.setState({count: seconds})
+		this.setState({count: seconds, countDownStatus: 'started'})
 	}
 
 /*-------------------------------------------------------*/
 	render() {
 		let {count} = this.state
-		return (
+		return ( 
 			<div >
 				<Clock totalSeconds={count}></Clock>
 				<CountDownForm onSetCountDown={this.handleSetCountDown}></CountDownForm>				
